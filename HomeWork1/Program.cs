5b1ee527749c62faa871174069d6572ea910531a
«Andrey_Istomin» <an.istomin@gmail.com>
2024-02-19 01:07:49 +0600

    Решение HomeWork1

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+*/
+
+
+while (true)
+{
+    Console.Write("Введите целое число или q для выхода: ");
+    string input = Console.ReadLine();
+
+    if (input == "q")
+    {
+        break;
+    }
+
+    int number;
+    if (int.TryParse(input, out number))
+    {
+        int sum = 0;
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        if (sum % 2 == 0)
+        {
+            Console.WriteLine("[STOP]");
+            break;
+        }
+    }
+
+    else
+    {
+        Console.WriteLine("Неккоретный ввод. Пожалуйста, введите целое число или 'q'. ");
+    }
+
+}

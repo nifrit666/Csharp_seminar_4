afcf21528ca7a30fef0dac8bfcc6c0502ba56ed1
«Andrey_Istomin» <an.istomin@gmail.com>
2024-02-19 01:22:41 +0600

    Решение HomeWork3

--- HomeWork#3/Program.cs
+++ HomeWork#3/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿/*
+Задача 3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+*/
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+int size = array.Length;
+int count = 0;
+int index = 0;
+
+
+Console.Write("Исходный массив: ");
+foreach (int item in array)
+{
+    Console.Write(item + " ");
+}
+
+while (index < size / 2)
+{
+    int temp = array[index];
+    array[index] = array[size - 1 - count];
+    array[size - 1 - count] = temp;
+    count += 1;
+    index += 1;
+}
+
+Console.Write("\nПеревернутый массив: ");
+foreach (int item in array)
+{
+    Console.Write(item + " ");
+}
